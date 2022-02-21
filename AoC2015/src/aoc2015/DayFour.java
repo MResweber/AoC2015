57635e0160c4e83c0b7adbb4e322aa0d0dac7175
Matthew Resweber <MaCResweber@gmail.com>
2022-02-21 14:48:51 -0500

    Finished Day Fours

--- AoC2015/src/aoc2015/DayFour.java
+++ AoC2015/src/aoc2015/DayFour.java
@@ -1,5 +1,9 @@
 package aoc2015;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class DayFour extends Day {
 
 	public DayFour(String inputPath) {
@@ -8,8 +12,38 @@ public class DayFour extends Day {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
+		String input = inputScanner.nextLine();
+		boolean found = false;
+		boolean found2 = false;
+		int num = 0;
+		
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		
+		while (!found || !found2) {
+			String key = input + num;
+			byte[] thing = md.digest(key.getBytes());
+			BigInteger number = new BigInteger(1, thing);
+			String result = number.toString(16);
+			while (result.length() < 32) {
+				result = "0" + result;
+			}
+			
+			if (result.substring(0, 5).equals("00000")) {
+				if (!found) System.out.println("Part 1 - Num: " + num);
+				found = true;
+			}
+			
+			if (result.substring(0, 6).equals("000000")) {
+				System.out.println("Part 2 - Num: " + num);
+				found2 = true;
+			}
+			num++;
+		}
 	}
 
 }
